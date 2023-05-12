068b8460581fee8ddf880d246be7a5dff6c65c5c
Koushik niyogi <koushikniyogionlinework@gmail.com>
2023-05-12 13:16:54 +0530

    added login register pagenotfound and nevbar

--- frontend/src/Routes/MainRoutes.tsx
+++ frontend/src/Routes/MainRoutes.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Routes,Route} from "react-router-dom"
+import Home from '../Pages/Home'
+import Login from '../Pages/Login'
+import Signup from '../Pages/Signup'
+import PagenotFound from '../Pages/PagenotFound'
 
 const MainRoutes = () => {
   return (
-    <div>MainRoutes</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="*" element={<PagenotFound/>}/>
+    </Routes>
   )
 }
 
